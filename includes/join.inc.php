64594f418d21ad556681ce64cf7fd402fed416b9
CarlosMateoM <carlosmateo484@gmail.com>
2022-10-17 13:45:30 -0500

    validacion de registro

--- includes/join.inc.php
+++ includes/join.inc.php
@@ -1,6 +1,49 @@
 <?php 
 
-echo 'formulario de registro';
+if(!isset($_POST['register-btn'])){
+    header("location: ./../join.php");
+    exit();
+}
+
+$id_power = $_POST['id_power'];
+$id_identificacion = $_POST['id_identificacion'];
+$username = $_POST['username'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$re_password = $_POST['re-password'];
 
 
-?>
+
+if(empty($id_power) || empty($id_identificacion) 
+|| empty($username) || empty($name) || empty($email
+|| empty($password) || empty($re_password))){
+    header("location: ./../join.php?error=emptyfields");
+    exit();
+} 
+else if (!is_numeric($id_power)) {
+    header("location: ./../join.php?error=id_power_wrong&id=$id_identificacion&username=$username&email=$email&name=$name");
+    exit();
+} 
+else if(!is_numeric($id_identificacion)){
+    header("location: ./../join.php?error=id_wrong&idpower=$id_power&username=$username&email=$email&name=$name");
+    exit();
+}
+else if(!preg_match("/^[a-zA-Z]*[0-9]*$/", $username )){
+    header("location: ./../join.php?error=username_wrong&id=$id_identificacion&idpower=$id_power&email=$email&name=$name");
+    exit();
+}
+else if(!preg_match("/^[a-zA-Z ]*$/", $name)){
+    header("location: ./../join.php?error=name_wrong&id=$id_identificacion&idpower=$id_power&email=$email&username=$username");
+    exit();
+}
+else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    header("location: ./../join.php?error=email_wrong&id=$id_identificacion&idpower=$id_power&email=$email&name=$name");
+    exit();
+}
+else if($password !== $re_password){
+    header("location: ./../join.php?error=password_wrong&id=$id_identificacion&idpower=$id_power&email=$email&name=$name&username=$username");
+    exit();
+}
+
+
--- join.php
+++ join.php
@@ -1,47 +1,125 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Join</title>
-    <link rel="stylesheet" href="./style/join.css"> 
+    <link rel="stylesheet" href="./style/join.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <main class="main">
         <div class="join-box">
             <h2 class="title-SignUp">Join</h2>
             <form class="join-form" action="./includes/join.inc.php" method="post">
-                
+
+                <?php
+                if (isset($_GET['error'])) {
+                    $error = $_GET['error'];
+                    if ($error == "emptyfields") {
+                        echo "<p style='color: red'> llene todos los campos </p>";
+                    }else if($error == "id_power_wrong"){
+                        echo "<p style='color: red'> ¡ID Power Campus no valido! </p>";
+                    }else if($error == "id_wrong"){
+                        echo "<p style='color: red'> ¡Número de identificacion no valido! </p>";
+                    }else if($error == "username_wrong"){
+                        echo "<p style='color: red'> ¡Username no valido! </p>";
+                    }else if($error == "name_wrong"){
+                        echo "<p style='color: red'> ¡Nombre no valido! </p>";
+                    }else if($error == "email_wrong"){
+                        echo "<p style='color: red'> ¡Email no valido! </p>";
+                    }else if ($error == "password_wrong"){
+                        echo "<p style='color: red'> ¡Password no coincide! </p>";
+                    } 
+                }
+
+                ?>
+
                 <div class="join-input">
-                    <input type="number" id="IDP" name="" required="">
+                    <?php 
+                    
+                    if(isset($_GET['idpower'])){
+                        echo "<input type='number' id='IDP' name='id_power' required='' value=".$_GET['idpower'].">";
+                    }else {
+                        echo "<input type='number' id='IDP' name='id_power' required=''>";
+                    }
+                    
+                    ?>
+                    
                     <label for="IDP">ID PowerCampus</label>
                 </div>
 
                 <div class="join-input">
-                    <input type="text" id="nombre" name="" required="">
-                    <label for="nombre">Name</label>
-                </div>
-                
-                <div class="join-input">
-                    <input type="number" id="DNI" name="" required="">
-                    <label for="DNI">identification number</label>
+                <?php 
+                    
+                    if(isset($_GET['id'])){
+                        echo "<input type='number' id='DNI' name='id_identificacion' required='' value=".$_GET['id'].">";
+                    }else {
+                        echo "<input type='number' id='DNI' name='id_identificacion' required=''>";
+                    }
+                    
+                    ?>
+
+                    
+                    <label for="DNI">Identification number</label>
                 </div>
 
                 <div class="join-input">
-                    <input type="text" id="usuario" name="" required="">
+                    <?php 
+                    
+                    if(isset($_GET['username'])){
+                        echo "<input type='text' id='usuario' name='username' required='' value=".$_GET['username'].">";
+                    }else {
+                        echo "<input type='text' id='usuario' name='username' required=''>";
+                    }
+                    
+                    ?>
                     <label for="usuario">Enter a username</label>
                 </div>
 
                 <div class="join-input">
-                    <input type="password" id="password" name="" required="">
+                <?php 
+                    
+                    if(isset($_GET['name'])){
+                        echo "<input type='text' id='nombre' name='name' required='' value=".$_GET['name'].">";
+                    }else {
+                        echo "<input type='text' id='nombre' name='name' required=''>";
+                    }
+                    
+                    ?>
+                    <label for="nombre">Name</label>
+                </div>
+
+                <div class="join-input">
+                    <?php 
+                    
+                        if(isset($_GET['email'])){
+                            echo "<input type='email' id='usuario' name='email' required=''value=".$_GET['email'].">";
+                        }else {
+                            echo "<input type='email' id='usuario' name='email' required=''>";
+                        }
+                    
+                    ?>
+
+                    <label for="usuario">Enter a email</label>
+                </div>
+
+                <div class="join-input">
+                    <input type="password" id="password" name="password" required="">
                     <label for="password">Create a password</label>
                 </div>
 
+                <div class="join-input">
+                    <input type="password" id="password" name="re-password" required="">
+                    <label for="password">Repeat password</label>
+                </div>
+
                 <div class="button-join">
-                    <input id="join" href="#">
+                    <input type="submit" id="join" name="register-btn" value="Register">
                 </div>
 
                 <div class="SignIn-content">
@@ -52,4 +130,5 @@
         </div>
     </main>
 </body>
+
 </html>
